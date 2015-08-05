d6f0f33d951190b9a8fe49632155a80207366194
Simone <simone.clclr@gmail.com>
2015-08-05 14:17:44 +0200

    created tests for Fol data structures

--- src/test/java/FolFormulaBuildingTest.java
+++ src/test/java/FolFormulaBuildingTest.java
@@ -1,3 +1,5 @@
+import formula.fol.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -6,7 +8,38 @@ import org.junit.Test;
 public class FolFormulaBuildingTest {
 
 	@Test
-	public static void testFolBuilding(){
+	public void testFolBuilding(){
+
+		//Building very simple formulas
+		//P(x)
+
+		FolPredicate p = new FolPredicate("P", 1);
+		FolConstant x = new FolConstant("x");
+		FolAtom px = new FolAtom(p, x);
+
+		String builtFormula = px.toString();
+
+		Assert.assertEquals("", "P(x)", builtFormula);
+
+		System.out.println("\nBuilt formula: " + builtFormula);
+
+		//More difficult
+		//P(x) && Q(a, b, ?c)
+
+		FolPredicate q = new FolPredicate("Q", 3);
+		FolConstant a = new FolConstant("a");
+		FolConstant b = new FolConstant("b");
+		FolVariable c = new FolVariable("c");
+		FolAtom qabc = new FolAtom(q, a, b , c);
+
+		FolFormula pANDq = new FolAndFormula(px, qabc);
+
+		builtFormula = pANDq.toString();
+
+		Assert.assertEquals("", "(P(x)) AND (Q(a, b, ?c))", builtFormula);
+
+		System.out.println("\nBuilt formula: " + builtFormula);
+
 
 	}
 
