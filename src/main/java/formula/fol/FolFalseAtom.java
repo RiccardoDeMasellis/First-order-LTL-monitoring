6002771f864707e9520a6deb03addf81b82dbe97
Simone <simone.clclr@gmail.com>
2015-08-13 13:11:21 +0200

    Added support for false and true atoms in FoFormulaParser grammar (for local formulas); updated local formula visitors. Renamed true and false atom classes in fol formula class structure

--- src/main/java/formula/fol/FolFalseAtom.java
+++ src/main/java/formula/fol/FolFalseAtom.java
@@ -3,7 +3,7 @@ package formula.fol;
 /**
  * Created by Simone Calciolari on 05/08/15.
  */
-public class FolFalseFormula extends FolAtomicFormula {
+public class FolFalseAtom extends FolAtomicFormula {
 
 	public String toString(){
 		return "FALSE";
